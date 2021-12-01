0572ef8fd1828552b60e68d5bfe07983b51cbabc
Florencia <floopis.rivas2@gmail.com>
2021-12-01 16:46:18 -0300

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+let btnmenu = document.getElementById('btnMenu');
+let menu = document.getElementById('menu');
+
+btnmenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+
